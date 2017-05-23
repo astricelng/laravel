57a1eb3e43cacc7ea2def2258c9449c4af0e9bb3
astricelng <astricelng@gmail.com>
2017-05-23 07:57:07 -0500

    segundo commit

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use App\Http\Requests\CreatePostRequest;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
     public function index(){
 
-    	$posts = Post::all();
+    	//$posts = Post::all();
+
+        $posts = Post::orderBy('id','desc')->get();
 
     	return view('posts.index')->with(['posts'=> $posts]);
     }
@@ -17,6 +20,27 @@ class PostsController extends Controller
     public function show(Post $post){
 
     	return view('posts.show')->with(['post'=> $post]);
+    }
+
+    public function create(){
+
+        return view('posts.create');
+    }
+
+    public function store(CreatePostRequest $request){
+
+        //$request->get('title');
+        //$request->title;
+
+        /*$post = new Post;
+        $post->title = $request->get('title');
+        $post->description = $request->get('description');
+        $post->url = $request->get('url');
+        $post->save();
+*/
+        $post = Post::create($request->only('title', 'description', 'url'));
+
+        return redirect()->route('posts_path');
     }
 
     // Cuando no se coloca la variable de entrada como tipo Post
--- routes/web.php
+++ routes/web.php
@@ -46,4 +46,9 @@ Route::get('/hola/{nombre}', 'HolaController@hola');
 
 Route::name('posts_path')->get('/posts', 'PostsController@index');
 
+Route::name('create_post_path')->get('/posts/create', 'PostsController@create');
+
+Route::name('store_post_path')->post('/posts', 'PostsController@store');
+
 Route::name('post_path')->get('/posts/{post}', 'PostsController@show');
+
